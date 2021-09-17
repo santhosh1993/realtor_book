af9768a1b237ebdc0e84071874364614d25d46cc
Santhosh <namp.santhosh@gmail.com>
2021-09-17 10:26:11 +0530

    added database

--- lib/src/realtor_book_app.dart
+++ lib/src/realtor_book_app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:realtor_book/src/config/router.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class RealtorBookApp extends StatelessWidget {
   @override
--- lib/src/splash/splash_controller.dart
+++ lib/src/splash/splash_controller.dart
@@ -1,12 +1,41 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:realtor_book/src/config/router.dart';
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    Future.delayed(Duration(seconds: 1), () {
-      Get.offAllNamed(Routes.contentForm);
-    });
+    checkDataBase();
+  }
+
+  checkDataBase() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final database =
+        openDatabase(join(await getDatabasesPath(), 'realtor_book_database.db'),
+            onCreate: (db, version) {
+              return createTables(db);
+            },
+            version: 1,
+            onUpgrade: (db, oldVersion, newVerison) {
+              Future.delayed(Duration(seconds: 1), () {
+                Get.offAllNamed(Routes.contentForm);
+              });
+            });
+  }
+
+  Future<void> createTables(Database db) async {
+    await db.execute(
+        'CREATE TABLE user(id INTEGER PRIMARY KEY, name TEXT, phone TEXT)');
+    await db.execute(
+        'CREATE TABLE property(id INTEGER PRIMARY KEY, userId INTEGER)');
+    await db.execute(
+        'CREATE TABLE detail(id INTEGER PRIMARY KEY, propertyId INTEGER, title TEXT, description TEXT)');
+    await db.execute(
+        'CREATE TABLE file(id INTEGER PRIMARY KEY, propertyId INTEGER, fileName TEXT, fileType INTEGER)');
   }
 }
